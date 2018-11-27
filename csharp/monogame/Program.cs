225ebff73e6f5e930223dcda0a2b612986eeef17
Johan Lindfors <johan.lindfors@coderox.se>
2018-11-27 19:55:28 +0100

    Initial commit of csharp with monogame

---
+++ csharp/monogame/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace WinFormTest
+{
+    public static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            using (var game = new SnakeGame())
+                game.Run();
+        }
+    }
+}
---
+++ csharp/monogame/SnakeGame.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace WinFormTest
+{
+    public class SnakeGame : Game
+    {
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+        Texture2D rectTexture;
+        
+        public SnakeGame()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+        }
+
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            // Create a new SpriteBatch, which can be used to draw textures.
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            var data = new Color[20 * 20];
+            rectTexture = new Texture2D(GraphicsDevice, 20, 20);
+
+            for (int i = 0; i < data.Length; ++i) 
+                data[i] = Color.Red;
+
+            rectTexture.SetData(data);
+        }
+
+        protected override void UnloadContent()
+        {
+            // TODO: Unload any non ContentManager content here
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            // TODO: Add your update logic here
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            var position = new Vector2(100,100);
+
+            spriteBatch.Begin();
+            spriteBatch.Draw(rectTexture, position, Color.White);
+            spriteBatch.End();
+
+            base.Draw(gameTime);
+        }
+    }
+}
