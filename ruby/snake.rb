4e42c792f61c1f687ce7ed70d6fd9f28de995cd2
Johan Lindfors <johan.lindfors@coderox.se>
2018-12-01 23:39:45 +0100

    Cleaner ruby

--- ruby/snake.rb
+++ ruby/snake.rb
@@ -4,60 +4,43 @@ SCREEN_SIZE = 20
 SPRITE_SIZE = 20
 INITIAL_TAIL = 5
 FRAMES_PER_SECOND = 15
+RED = 0xffff0000
+GREEN = 0xff00ff00
+BLUE = 0xff0000ff
 
 class Drawable
     attr_accessor :x, :y
 
-    def initialize
-        @x = @y = 0
-        @color = 0xffffffff
+    def initialize x, y, color
+        @x = x
+        @y = y
+        @color = color
+        @width = SPRITE_SIZE - 1
+        @height = SPRITE_SIZE - 1
     end
 
     def draw
-        Gosu.draw_rect(x * SPRITE_SIZE + 1, y * SPRITE_SIZE + 1, SPRITE_SIZE - 1, SPRITE_SIZE - 1, @color)
-    end
-end
-
-class Apple < Drawable
-    def initialize
-        @x = @y = 3
-        @color = 0xffff0000
+        Gosu.draw_rect x * SPRITE_SIZE + 1, y * SPRITE_SIZE + 1, @width, @height, @color
     end
 end
 
 class Snake < Drawable
     attr_accessor :dx, :dy, :tail, :trail
 
-    def initialize
-        @x = @y = 10
+    def initialize x, y, color
+        super x,y,color
         @dx = @dy = 0
-        @color = 0xff00ff00
         @trail = Array.new
         @tail = INITIAL_TAIL
     end
 
-    def checkCollision(x, y)
+    def checkCollision x, y
         @trail.each do |element| 
-            if x == element.x and y == element.y and tail > INITIAL_TAIL
+            if x == element.x and y == element.y
                 return true
             end
         end
         return false
-    end
-
-    def clampToScreen
-        if @x < 0
-            @x = SCREEN_SIZE - 1
-        end
-        if @x >= SCREEN_SIZE
-            @x = 0
-        end
-        if @y < 0
-            @y = SCREEN_SIZE - 1
-        end
-        if @y >= SCREEN_SIZE
-            @y = 0
-        end
     end
 
     def update
@@ -70,7 +53,8 @@ class Snake < Drawable
             @dx = @dy = 0
         end
         
-        clampToScreen
+        @x = @x % SCREEN_SIZE
+        @y = @y % SCREEN_SIZE
 
         @trail << Struct.new(:x, :y).new(@x, @y)
         while @trail.length > @tail
@@ -80,7 +64,7 @@ class Snake < Drawable
 
     def draw
         @trail.each do |element| 
-            Gosu.draw_rect(element.x * SPRITE_SIZE + 1, element.y * SPRITE_SIZE + 1, SPRITE_SIZE - 1, SPRITE_SIZE - 1, @color)
+            Gosu.draw_rect element.x * SPRITE_SIZE + 1, element.y * SPRITE_SIZE + 1, @width, @height, @color
         end
     end
 end
@@ -89,10 +73,10 @@ class SnakeWindow < Gosu::Window
     def initialize
         super SPRITE_SIZE * SCREEN_SIZE, SPRITE_SIZE * SCREEN_SIZE
         self.caption = 'Snake'
-        self.update_interval = 1000/FRAMES_PER_SECOND
+        self.update_interval = 1000 / FRAMES_PER_SECOND
 
-        @apple = Apple.new
-        @snake = Snake.new
+        @apple = Drawable.new 3, 10, RED
+        @snake = Snake.new 10, 10, GREEN
         @prng = Random.new
     end
 
@@ -100,7 +84,7 @@ class SnakeWindow < Gosu::Window
         loop do 
             @apple.x = @prng.rand SCREEN_SIZE
             @apple.y = @prng.rand SCREEN_SIZE
-            break @snake.checkCollision @apple.x, @apple.y
+            break unless @snake.checkCollision @apple.x, @apple.y
         end
     end
 
