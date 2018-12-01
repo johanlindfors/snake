ac192b57211f0a4b39d84e543bc42772c55d65f3
Johan Lindfors <johan.lindfors@coderox.se>
2018-12-01 09:20:00 +0100

    Python version cleaner, learned about immutable int

--- python/snake.py
+++ python/snake.py
@@ -9,104 +9,112 @@ GREEN = 0, 255, 0
 MIDDLE = math.floor(SCREEN_SIZE/2)
 THIRD = math.floor(SCREEN_SIZE/3)
 INITIAL_TAIL = 5
-FPS = 15
+FRAMES_PER_SECOND = 15
 
-width = SCREEN_SIZE * SPRITE_SIZE
-height = SCREEN_SIZE * SPRITE_SIZE
-size = width, height 
+def handle_input(speed):
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT: 
+            sys.exit()
+        elif event.type == KEYDOWN:
+            if speed.x == 0:
+                if event.key == K_LEFT:
+                    speed.x = -1
+                    speed.y = 0
+                elif event.key == K_RIGHT:
+                    speed.x = 1
+                    speed.y = 0
+            if speed.y == 0:
+                if event.key == K_UP:
+                    speed.x = 0
+                    speed.y = -1
+                elif event.key == K_DOWN:
+                    speed.x = 0
+                    speed.y = 1
+            if event.key == K_ESCAPE:
+                sys.exit()              
 
-screen = pygame.display.set_mode(size)
-clock = pygame.time.Clock()
+def check_collision(obj, trail):
+    for body in trail:
+        if body == obj:
+            return True
+    return False
 
-class SnakeGame(object):
-    def __init__(self):
-        pygame.init()
-        
-        self._player = pygame.Vector2(MIDDLE, MIDDLE)
-        self._apple = pygame.Vector2(THIRD, THIRD)
-        self._speed = pygame.Vector2()
-        self._trail = []
-        self._tail = INITIAL_TAIL
+def generate_apple(apple, trail):
+    while True:
+        apple.x = random.randint(0, SCREEN_SIZE - 1)
+        apple.y = random.randint(0, SCREEN_SIZE - 1)
+        if check_collision(apple, trail) == False:
+            break
 
-    def handle_input(self):
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT: 
-                sys.exit()
-            elif event.type == KEYDOWN:
-                if self._speed.x == 0:
-                    if event.key == K_LEFT:
-                        self._speed.x = -1
-                        self._speed.y = 0
-                    elif event.key == K_RIGHT:
-                        self._speed.x = 1
-                        self._speed.y = 0
-                if self._speed.y == 0:
-                    if event.key == K_UP:
-                        self._speed.x = 0
-                        self._speed.y = -1
-                    elif event.key == K_DOWN:
-                        self._speed.x = 0
-                        self._speed.y = 1                
+def check_pickup(apple, player, trail, tail):
+    if apple == player:
+        tail += 1
+        generate_apple(apple, trail)
+    return tail
 
-    def check_collision(self):
-        for body in self._trail:
-            if body == self._player:
-                if self._tail > INITIAL_TAIL:
-                    print(self._tail - INITIAL_TAIL)
-                self._tail = INITIAL_TAIL
-                self._trail = []
-                self._player = pygame.Vector2(MIDDLE, MIDDLE)
-                self._speed = pygame.Vector2(0,0)
+def update(player, apple, trail, tail, speed):
+    player.x += speed.x
+    player.y += speed.y
 
-    def generate_apple(self):
-        self._apple.x = random.randint(0, SCREEN_SIZE - 1)
-        self._apple.y = random.randint(0, SCREEN_SIZE - 1)
+    if player.x < 0:
+        player.x = SCREEN_SIZE - 1
+    if player.y < 0:
+        player.y = SCREEN_SIZE - 1
+    if player.x > SCREEN_SIZE - 1:
+        player.x = 0
+    if player.y > SCREEN_SIZE - 1:
+        player.y = 0
 
-    def check_pickup(self):
-        if self._apple == self._player:
-            self._tail += 1
-            self.generate_apple()
+    if check_collision(player, trail):
+        if tail > INITIAL_TAIL:
+            print(tail - INITIAL_TAIL)
+        tail = INITIAL_TAIL
+        trail.clear()
+        player.x = MIDDLE
+        player.y = MIDDLE
+        speed.x = 0
+        speed.y = 0
 
-    def update(self):
-        self._player.x += self._speed.x
-        self._player.y += self._speed.y
+    tail = check_pickup(apple, player, trail, tail)
 
-        if self._player.x < 0:
-            self._player.x = SCREEN_SIZE - 1
-        if self._player.y < 0:
-            self._player.y = SCREEN_SIZE - 1
-        if self._player.x > SCREEN_SIZE - 1:
-            self._player.x = 0
-        if self._player.y > SCREEN_SIZE - 1:
-            self._player.y = 0
+    trail.append(pygame.Vector2(player.x, player.y))
+    while len(trail) > tail:
+        trail.pop(0)
+    return tail
 
-        self.check_collision()
-        self.check_pickup()
+def draw(screen, player, apple, trail):
+    # clear screen
+    screen.fill(BLACK)
 
-        self._trail.append(pygame.Vector2(self._player.x, self._player.y))
-        while len(self._trail) > self._tail:
-            self._trail.pop(0)
+    # draw snake
+    for body in trail:
+        body_rect = Rect(body.x * SPRITE_SIZE + 1, body.y * SPRITE_SIZE + 1, SPRITE_SIZE - 1, SPRITE_SIZE - 1)
+        pygame.draw.rect(screen, GREEN, body_rect)
 
-    def draw(self):
-        # clear screen
-        screen.fill(BLACK)
+    # draw apple
+    apple_rect = Rect(apple.x * SPRITE_SIZE + 1, apple.y * SPRITE_SIZE + 1, SPRITE_SIZE - 1,  SPRITE_SIZE - 1)
+    pygame.draw.rect(screen, RED, apple_rect)
 
-        # draw snake
-        for body in self._trail:
-            body_rect = Rect(body.x * SPRITE_SIZE + 1, body.y * SPRITE_SIZE + 1, SPRITE_SIZE - 1, SPRITE_SIZE - 1)
-            pygame.draw.rect(screen, GREEN, body_rect)
+def run():
+    width = SCREEN_SIZE * SPRITE_SIZE
+    height = SCREEN_SIZE * SPRITE_SIZE
+    size = width, height 
 
-        # draw apple
-        apple_rect = Rect(self._apple.x * SPRITE_SIZE + 1, self._apple.y * SPRITE_SIZE + 1, SPRITE_SIZE - 1,  SPRITE_SIZE - 1)
-        pygame.draw.rect(screen, RED, apple_rect)
+    screen = pygame.display.set_mode(size)
+    clock = pygame.time.Clock()
 
-    def run(self):
-        while True:
-            clock.tick(FPS)
-            self.handle_input()
-            self.update()
-            self.draw()
-            pygame.display.flip()
+    player = pygame.Vector2(MIDDLE, MIDDLE)
+    apple = pygame.Vector2(THIRD, THIRD)
+    speed = pygame.Vector2()
+    trail = []
+    tail = INITIAL_TAIL
 
-game = SnakeGame()
-game.run()
+    pygame.init()
+    while True:
+        clock.tick(FRAMES_PER_SECOND)
+        handle_input(speed)
+        tail = update(player, apple, trail, tail, speed)
+        draw(screen, player, apple, trail)
+        pygame.display.flip()
+
+run()
