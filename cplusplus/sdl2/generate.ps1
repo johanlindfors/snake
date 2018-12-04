f7c5a8aedd02fdb423c15e993610ece47020cf91
Johan Lindfors <Johan.Lindfors@coderox.se>
2018-12-04 19:08:55 +0100

    Fixes for .ps1 files, still need proper path to vcpkg

--- cplusplus/sdl2/generate.ps1
+++ cplusplus/sdl2/generate.ps1
@@ -1,3 +1,7 @@
-mkdir build
+$path = "build"
+If(!(test-path $path))
+{
+      New-Item -ItemType Directory -Force -Path $path
+}
 cd build
-cmake -DCMAKE_TOOLCHAIN_FILE="C:\\repos\\thirdparty\\vcpkg\\scripts\\buildsystems\\vcpkg.cmake" -DCMAKE_BUILD_TYPE=Debug ../
+cmake -DCMAKE_TOOLCHAIN_FILE="D:\\repos\\thirdparty\\vcpkg\\scripts\\buildsystems\\vcpkg.cmake" -DCMAKE_BUILD_TYPE=Debug ../
