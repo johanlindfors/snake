355e709c206088defbc03d98562e40e637c2045a
Johan Lindfors <johan.lindfors@coderox.se>
2018-12-17 11:40:41 +0100

    Now working, need input

--- cplusplus/dx/include/StepTimer.h
+++ cplusplus/dx/include/StepTimer.h
@@ -7,183 +7,176 @@
 #include <cmath>
 #include <exception>
 #include <stdint.h>
+#include <windows.h>
 
-namespace DX
-{
-    // Helper class for animation and simulation timing.
-    class StepTimer
-    {
-    public:
-        StepTimer() noexcept(false) :
-            m_elapsedTicks(0),
-            m_totalTicks(0),
-            m_leftOverTicks(0),
-            m_frameCount(0),
-            m_framesPerSecond(0),
-            m_framesThisSecond(0),
-            m_qpcSecondCounter(0),
-            m_isFixedTimeStep(false),
-            m_targetElapsedTicks(TicksPerSecond / 60)
-        {
-            if (!QueryPerformanceFrequency(&m_qpcFrequency))
-            {
-                throw std::exception( "QueryPerformanceFrequency" );
-            }
 
-            if (!QueryPerformanceCounter(&m_qpcLastTime))
-            {
-                throw std::exception( "QueryPerformanceCounter" );
-            }
+namespace DX {
+// Helper class for animation and simulation timing.
+class StepTimer {
+public:
+  StepTimer() noexcept(false)
+      : m_elapsedTicks(0), m_totalTicks(0), m_leftOverTicks(0), m_frameCount(0),
+        m_framesPerSecond(0), m_framesThisSecond(0), m_qpcSecondCounter(0),
+        m_isFixedTimeStep(false), m_targetElapsedTicks(TicksPerSecond / 60) {
+    if (!QueryPerformanceFrequency(&m_qpcFrequency)) {
+      throw std::exception("QueryPerformanceFrequency");
+    }
 
-            // Initialize max delta to 1/10 of a second.
-            m_qpcMaxDelta = static_cast<uint64_t>(m_qpcFrequency.QuadPart / 10);
-        }
+    if (!QueryPerformanceCounter(&m_qpcLastTime)) {
+      throw std::exception("QueryPerformanceCounter");
+    }
 
-        // Get elapsed time since the previous Update call.
-        uint64_t GetElapsedTicks() const					{ return m_elapsedTicks; }
-        double GetElapsedSeconds() const					{ return TicksToSeconds(m_elapsedTicks); }
+    // Initialize max delta to 1/10 of a second.
+    m_qpcMaxDelta = static_cast<uint64_t>(m_qpcFrequency.QuadPart / 10);
+  }
 
-        // Get total time since the start of the program.
-        uint64_t GetTotalTicks() const						{ return m_totalTicks; }
-        double GetTotalSeconds() const						{ return TicksToSeconds(m_totalTicks); }
+  // Get elapsed time since the previous Update call.
+  uint64_t GetElapsedTicks() const { return m_elapsedTicks; }
+  double GetElapsedSeconds() const { return TicksToSeconds(m_elapsedTicks); }
 
-        // Get total number of updates since start of the program.
-        uint32_t GetFrameCount() const						{ return m_frameCount; }
+  // Get total time since the start of the program.
+  uint64_t GetTotalTicks() const { return m_totalTicks; }
+  double GetTotalSeconds() const { return TicksToSeconds(m_totalTicks); }
 
-        // Get the current framerate.
-        uint32_t GetFramesPerSecond() const					{ return m_framesPerSecond; }
+  // Get total number of updates since start of the program.
+  uint32_t GetFrameCount() const { return m_frameCount; }
 
-        // Set whether to use fixed or variable timestep mode.
-        void SetFixedTimeStep(bool isFixedTimestep)			{ m_isFixedTimeStep = isFixedTimestep; }
+  // Get the current framerate.
+  uint32_t GetFramesPerSecond() const { return m_framesPerSecond; }
 
-        // Set how often to call Update when in fixed timestep mode.
-        void SetTargetElapsedTicks(uint64_t targetElapsed)	{ m_targetElapsedTicks = targetElapsed; }
-        void SetTargetElapsedSeconds(double targetElapsed)	{ m_targetElapsedTicks = SecondsToTicks(targetElapsed); }
+  // Set whether to use fixed or variable timestep mode.
+  void SetFixedTimeStep(bool isFixedTimestep) {
+    m_isFixedTimeStep = isFixedTimestep;
+  }
 
-        // Integer format represents time using 10,000,000 ticks per second.
-        static const uint64_t TicksPerSecond = 10000000;
+  // Set how often to call Update when in fixed timestep mode.
+  void SetTargetElapsedTicks(uint64_t targetElapsed) {
+    m_targetElapsedTicks = targetElapsed;
+  }
+  void SetTargetElapsedSeconds(double targetElapsed) {
+    m_targetElapsedTicks = SecondsToTicks(targetElapsed);
+  }
 
-        static double TicksToSeconds(uint64_t ticks)		{ return static_cast<double>(ticks) / TicksPerSecond; }
-        static uint64_t SecondsToTicks(double seconds)		{ return static_cast<uint64_t>(seconds * TicksPerSecond); }
+  // Integer format represents time using 10,000,000 ticks per second.
+  static const uint64_t TicksPerSecond = 10000000;
 
-        // After an intentional timing discontinuity (for instance a blocking IO operation)
-        // call this to avoid having the fixed timestep logic attempt a set of catch-up 
-        // Update calls.
+  static double TicksToSeconds(uint64_t ticks) {
+    return static_cast<double>(ticks) / TicksPerSecond;
+  }
+  static uint64_t SecondsToTicks(double seconds) {
+    return static_cast<uint64_t>(seconds * TicksPerSecond);
+  }
 
-        void ResetElapsedTime()
-        {
-            if (!QueryPerformanceCounter(&m_qpcLastTime))
-            {
-                throw std::exception("QueryPerformanceCounter");
-            }
+  // After an intentional timing discontinuity (for instance a blocking IO
+  // operation) call this to avoid having the fixed timestep logic attempt a set
+  // of catch-up Update calls.
 
-            m_leftOverTicks = 0;
-            m_framesPerSecond = 0;
-            m_framesThisSecond = 0;
-            m_qpcSecondCounter = 0;
-        }
+  void ResetElapsedTime() {
+    if (!QueryPerformanceCounter(&m_qpcLastTime)) {
+      throw std::exception("QueryPerformanceCounter");
+    }
 
-        // Update timer state, calling the specified Update function the appropriate number of times.
-        template<typename TUpdate>
-        void Tick(const TUpdate& update)
-        {
-            // Query the current time.
-            LARGE_INTEGER currentTime;
+    m_leftOverTicks = 0;
+    m_framesPerSecond = 0;
+    m_framesThisSecond = 0;
+    m_qpcSecondCounter = 0;
+  }
 
-            if (!QueryPerformanceCounter(&currentTime))
-            {
-                throw std::exception( "QueryPerformanceCounter" );
-            }
+  // Update timer state, calling the specified Update function the appropriate
+  // number of times.
+  template <typename TUpdate> void Tick(const TUpdate &update) {
+    // Query the current time.
+    LARGE_INTEGER currentTime;
 
-            uint64_t timeDelta = currentTime.QuadPart - m_qpcLastTime.QuadPart;
+    if (!QueryPerformanceCounter(&currentTime)) {
+      throw std::exception("QueryPerformanceCounter");
+    }
 
-            m_qpcLastTime = currentTime;
-            m_qpcSecondCounter += timeDelta;
+    uint64_t timeDelta = currentTime.QuadPart - m_qpcLastTime.QuadPart;
 
-            // Clamp excessively large time deltas (e.g. after paused in the debugger).
-            if (timeDelta > m_qpcMaxDelta)
-            {
-                timeDelta = m_qpcMaxDelta;
-            }
+    m_qpcLastTime = currentTime;
+    m_qpcSecondCounter += timeDelta;
 
-            // Convert QPC units into a canonical tick format. This cannot overflow due to the previous clamp.
-            timeDelta *= TicksPerSecond;
-            timeDelta /= m_qpcFrequency.QuadPart;
+    // Clamp excessively large time deltas (e.g. after paused in the debugger).
+    if (timeDelta > m_qpcMaxDelta) {
+      timeDelta = m_qpcMaxDelta;
+    }
 
-            uint32_t lastFrameCount = m_frameCount;
+    // Convert QPC units into a canonical tick format. This cannot overflow due
+    // to the previous clamp.
+    timeDelta *= TicksPerSecond;
+    timeDelta /= m_qpcFrequency.QuadPart;
 
-            if (m_isFixedTimeStep)
-            {
-                // Fixed timestep update logic
+    uint32_t lastFrameCount = m_frameCount;
 
-                // If the app is running very close to the target elapsed time (within 1/4 of a millisecond) just clamp
-                // the clock to exactly match the target value. This prevents tiny and irrelevant errors
-                // from accumulating over time. Without this clamping, a game that requested a 60 fps
-                // fixed update, running with vsync enabled on a 59.94 NTSC display, would eventually
-                // accumulate enough tiny errors that it would drop a frame. It is better to just round 
-                // small deviations down to zero to leave things running smoothly.
+    if (m_isFixedTimeStep) {
+      // Fixed timestep update logic
 
-                if (static_cast<uint64_t>(std::abs(static_cast<int64_t>(timeDelta - m_targetElapsedTicks))) < TicksPerSecond / 4000)
-                {
-                    timeDelta = m_targetElapsedTicks;
-                }
+      // If the app is running very close to the target elapsed time (within 1/4
+      // of a millisecond) just clamp the clock to exactly match the target
+      // value. This prevents tiny and irrelevant errors from accumulating over
+      // time. Without this clamping, a game that requested a 60 fps fixed
+      // update, running with vsync enabled on a 59.94 NTSC display, would
+      // eventually accumulate enough tiny errors that it would drop a frame. It
+      // is better to just round small deviations down to zero to leave things
+      // running smoothly.
 
-                m_leftOverTicks += timeDelta;
+      if (static_cast<uint64_t>(std::abs(static_cast<int64_t>(
+              timeDelta - m_targetElapsedTicks))) < TicksPerSecond / 4000) {
+        timeDelta = m_targetElapsedTicks;
+      }
 
-                while (m_leftOverTicks >= m_targetElapsedTicks)
-                {
-                    m_elapsedTicks = m_targetElapsedTicks;
-                    m_totalTicks += m_targetElapsedTicks;
-                    m_leftOverTicks -= m_targetElapsedTicks;
-                    m_frameCount++;
+      m_leftOverTicks += timeDelta;
 
-                    update();
-                }
-            }
-            else
-            {
-                // Variable timestep update logic.
-                m_elapsedTicks = timeDelta;
-                m_totalTicks += timeDelta;
-                m_leftOverTicks = 0;
-                m_frameCount++;
+      while (m_leftOverTicks >= m_targetElapsedTicks) {
+        m_elapsedTicks = m_targetElapsedTicks;
+        m_totalTicks += m_targetElapsedTicks;
+        m_leftOverTicks -= m_targetElapsedTicks;
+        m_frameCount++;
 
-                update();
-            }
+        update();
+      }
+    } else {
+      // Variable timestep update logic.
+      m_elapsedTicks = timeDelta;
+      m_totalTicks += timeDelta;
+      m_leftOverTicks = 0;
+      m_frameCount++;
 
-            // Track the current framerate.
-            if (m_frameCount != lastFrameCount)
-            {
-                m_framesThisSecond++;
-            }
+      update();
+    }
 
-            if (m_qpcSecondCounter >= static_cast<uint64_t>(m_qpcFrequency.QuadPart))
-            {
-                m_framesPerSecond = m_framesThisSecond;
-                m_framesThisSecond = 0;
-                m_qpcSecondCounter %= m_qpcFrequency.QuadPart;
-            }
-        }
+    // Track the current framerate.
+    if (m_frameCount != lastFrameCount) {
+      m_framesThisSecond++;
+    }
 
-    private:
-        // Source timing data uses QPC units.
-        LARGE_INTEGER m_qpcFrequency;
-        LARGE_INTEGER m_qpcLastTime;
-        uint64_t m_qpcMaxDelta;
+    if (m_qpcSecondCounter >= static_cast<uint64_t>(m_qpcFrequency.QuadPart)) {
+      m_framesPerSecond = m_framesThisSecond;
+      m_framesThisSecond = 0;
+      m_qpcSecondCounter %= m_qpcFrequency.QuadPart;
+    }
+  }
 
-        // Derived timing data uses a canonical tick format.
-        uint64_t m_elapsedTicks;
-        uint64_t m_totalTicks;
-        uint64_t m_leftOverTicks;
+private:
+  // Source timing data uses QPC units.
+  LARGE_INTEGER m_qpcFrequency;
+  LARGE_INTEGER m_qpcLastTime;
+  uint64_t m_qpcMaxDelta;
 
-        // Members for tracking the framerate.
-        uint32_t m_frameCount;
-        uint32_t m_framesPerSecond;
-        uint32_t m_framesThisSecond;
-        uint64_t m_qpcSecondCounter;
+  // Derived timing data uses a canonical tick format.
+  uint64_t m_elapsedTicks;
+  uint64_t m_totalTicks;
+  uint64_t m_leftOverTicks;
 
-        // Members for configuring fixed timestep mode.
-        bool m_isFixedTimeStep;
-        uint64_t m_targetElapsedTicks;
-    };
-}
+  // Members for tracking the framerate.
+  uint32_t m_frameCount;
+  uint32_t m_framesPerSecond;
+  uint32_t m_framesThisSecond;
+  uint64_t m_qpcSecondCounter;
+
+  // Members for configuring fixed timestep mode.
+  bool m_isFixedTimeStep;
+  uint64_t m_targetElapsedTicks;
+};
+} // namespace DX
--- cplusplus/dx/src/main.cpp
+++ cplusplus/dx/src/main.cpp
@@ -9,24 +9,17 @@
 #include <list>
 // #include <memory>
 // #include <string>
-#include <windows.h>
-#include "../include/dx.h"
-#include "../include/DeviceResources.h"
+// #include "../include/DeviceResources.h"
 #include "../include/StepTimer.h"
-
-#include <DirectXColors.h>
+#include "../include/dx.h"
+#include <windows.h>
 
 using namespace KennyKerr;
 using namespace KennyKerr::Direct2D;
 
-Color const COLOR_WHITE(1.0f, 1.0f, 1.0f);
-Color const COLOR_BLUE(0.26f, 0.56f, 0.87f);
-
-static Factory1 factory;
-static DeviceContext target;
-static SolidColorBrush brush;
-static Dxgi::SwapChain1 swapChain;
-static DirectWrite::TextLayout textLayout;
+Color const COLOR_BLACK(0.0f, 0.0f, 0.0f);
+Color const COLOR_RED(1.0f, 0.0f, 0.0f);
+Color const COLOR_GREEN(0.0f, 1.0f, 0.0f);
 
 const int SPRITE_SIZE = 20;
 const int SCREEN_SIZE = 20;
@@ -37,238 +30,259 @@ const int SCREEN_WIDTH = SPRITE_SIZE * SCREEN_SIZE;
 const int SCREEN_HEIGHT = SPRITE_SIZE * SCREEN_SIZE;
 
 struct Point {
-  public:
-    int X;
-    int Y;
+public:
+  int X;
+  int Y;
 
-    Point(int x, int y) : X(x), Y(y) {}
+  Point(int x, int y) : X(x), Y(y) {}
 };
 
 class Snake {
-  private:
-    int x;
-    int y;
-    std::list<Point> trail;
+private:
+  int x;
+  int y;
+  std::list<Point> trail;
+  SolidColorBrush brush;
 
-  public:
-    int tail;
-    int dx;
-    int dy;
+public:
+  int tail;
+  int dx;
+  int dy;
 
-    Snake() {
-        trail = std::list<Point>();
-        tail = INITIAL_TAIL;
-        x = 10;
-        y = 10;
-        dx = 0;
-        dy = 1;
+  Snake() {
+    trail = std::list<Point>();
+    tail = INITIAL_TAIL;
+    x = 10;
+    y = 10;
+    dx = 0;
+    dy = 1;
+  }
+
+  void Initialize(DeviceContext target) {
+    brush = target.CreateSolidColorBrush(COLOR_GREEN);
+  }
+
+  bool CheckCollision(int x, int y) {
+    for (Point point : trail) {
+      if (point.X == x && point.Y == y) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  void Update() {
+    x = (x + dx + SCREEN_SIZE) % SCREEN_SIZE;
+    y = (y + dy + SCREEN_SIZE) % SCREEN_SIZE;
+
+    if (CheckCollision(x, y)) {
+      x = y = 10;
+      dx = dy = 0;
+      tail = INITIAL_TAIL;
     }
 
-    bool checkCollision(int x, int y) {
-        for (Point point : trail) {
-            if (point.X == x && point.Y == y) {
-                return true;
-            }
-        }
-        return false;
+    trail.push_back(Point(x, y));
+    while ((int)trail.size() > tail) {
+      trail.pop_front();
     }
+  }
 
-    void update() {
-        x = (x + dx + SCREEN_SIZE) % SCREEN_SIZE;
-        y = (y + dy + SCREEN_SIZE) % SCREEN_SIZE;
-
-        if (checkCollision(x, y)) {
-            x = y = 10;
-            dx = dy = 0;
-            tail = INITIAL_TAIL;
-        }
-
-        trail.push_back(Point(x, y));
-        while ((int)trail.size() > tail) {
-            trail.pop_front();
-        }
+  void Draw(DeviceContext target) {
+    // Render green filled quad
+    for (Point point : trail) {
+      auto left = point.X * SPRITE_SIZE + 1;
+      auto top = point.Y * SPRITE_SIZE + 1;
+      auto right = left + SPRITE_SIZE - 1;
+      auto bottom = top + SPRITE_SIZE - 1;
+      target.FillRectangle(RectF(left, top, right, bottom), brush);
     }
-
-    void draw() {
-        // Render green filled quad
-        for (Point point : trail) {
-        }
-    }
+  }
 };
 
 class Apple {
-  public:
-    int x;
-    int y;
+private:
+  SolidColorBrush brush;
 
-    Apple() {
-        x = 3;
-        y = 3;
-        std::srand(std::time(nullptr));
-    }
+public:
+  int x;
+  int y;
 
-    void reposition(int x, int y) {
-        this->x = x;
-        this->y = y;
-    }
+  Apple() {
+    x = 3;
+    y = 3;
+    std::srand(std::time(nullptr));
+  }
 
-    void draw() {
-        // Render red filled quad
-    }
+  void Initialize(DeviceContext target) {
+    brush = target.CreateSolidColorBrush(COLOR_RED);
+  }
+
+  void Reposition(int x, int y) {
+    this->x = x;
+    this->y = y;
+  }
+
+  void Draw(DeviceContext target) {
+    auto left = this->x * SPRITE_SIZE + 1;
+    auto top = this->y * SPRITE_SIZE + 1;
+    auto right = left + SPRITE_SIZE - 1;
+    auto bottom = top + SPRITE_SIZE - 1;
+    target.FillRectangle(RectF(left, top, right, bottom), brush);
+  }
 };
 
 class Game {
-  private:
-    std::unique_ptr<Snake> snake;
-    std::unique_ptr<Apple> apple;
-    std::unique_ptr<DX::DeviceResources> deviceResources;
-    DX::StepTimer timer;
+private:
+  std::unique_ptr<Snake> snake;
+  std::unique_ptr<Apple> apple;
+  Factory1 factory;
+  DeviceContext target;
+  SolidColorBrush brush;
+  Dxgi::SwapChain1 swapChain;
+  DirectWrite::TextLayout textLayout;
 
-  public:
-    Game() 
-        : snake(std::make_unique<Snake>())
-        , apple(std::make_unique<Apple>()) {
-            deviceResources = std::make_unique<DX::DeviceResources>();
-        }
+  DX::StepTimer timer;
 
-    ~Game() { }
+public:
+  Game() : snake(std::make_unique<Snake>()), apple(std::make_unique<Apple>()) {
+    factory = CreateFactory();
+    timer.SetFixedTimeStep(true);
+    timer.SetTargetElapsedSeconds(1.f / 15.f);
+  }
 
-    void Tick() {
-        timer.Tick([&]()
-        {
-            Update();
-        });
-        Draw();
+  ~Game() { factory.Reset(); }
+
+  void Tick() {
+    timer.Tick([&]() { Update(); });
+    Draw();
+  }
+
+  bool Initialize(HWND window, int width, int height) {
+    auto device = Direct3D::CreateDevice();
+    target = factory.CreateDevice(device).CreateDeviceContext();
+    auto dxgi = device.GetDxgiFactory();
+
+    Dxgi::SwapChainDescription1 description;
+    description.SwapEffect = Dxgi::SwapEffect::Discard;
+
+    swapChain = dxgi.CreateSwapChainForHwnd(device, window, description);
+
+    auto props = BitmapProperties1(
+        BitmapOptions::Target | BitmapOptions::CannotDraw,
+        PixelFormat(Dxgi::Format::B8G8R8A8_UNORM, AlphaMode::Ignore));
+
+    target.SetTarget(target.CreateBitmapFromDxgiSurface(swapChain, props));
+
+    auto const dpi = factory.GetDesktopDpi();
+    target.SetDpi(dpi, dpi);
+
+    apple->Initialize(target);
+    snake->Initialize(target);
+
+    return true;
+  }
+
+  bool HandleInput() { return true; }
+
+  void Update() {
+    snake->Update();
+    if (snake->CheckCollision(apple->x, apple->y)) {
+      snake->tail++;
+      int x = 0;
+      int y = 0;
+      do {
+        x = std::rand() % SCREEN_SIZE;
+        y = std::rand() % SCREEN_SIZE;
+      } while (snake->CheckCollision(x, y));
+      apple->Reposition(x, y);
     }
+  }
 
-    bool Initialize(HWND window, int width, int height) {
-        deviceResources->SetWindow(window, width, height);
+  void Draw() {
+    // Clear the window
+    target.BeginDraw();
 
-        deviceResources->CreateDeviceResources();
-        //CreateDeviceDependentResources();
+    target.Clear(COLOR_BLACK);
 
-        deviceResources->CreateWindowSizeDependentResources();
-        //CreateWindowSizeDependentResources();
+    apple->Draw(target);
+    snake->Draw(target);
 
-        return true; 
-    }
+    target.EndDraw();
 
-    bool HandleInput() { return true; }
-
-    void Update() {
-        snake->update();
-        if (snake->checkCollision(apple->x, apple->y)) {
-            snake->tail++;
-            int x = 0;
-            int y = 0;
-            do {
-                x = std::rand() % SCREEN_SIZE;
-                y = std::rand() % SCREEN_SIZE;
-            } while (snake->checkCollision(x, y));
-            apple->reposition(x, y);
-        }
-    }
-
-    void Draw() {
-        // Clear the window
-        auto context = deviceResources->GetD3DDeviceContext();
-        auto renderTarget = deviceResources->GetRenderTargetView();
-        auto depthStencil = deviceResources->GetDepthStencilView();
-
-        context->ClearRenderTargetView(renderTarget, DirectX::Colors::Black);
-        context->ClearDepthStencilView(depthStencil, D3D11_CLEAR_DEPTH | D3D11_CLEAR_STENCIL, 1.0f, 0);
-        context->OMSetRenderTargets(1, &renderTarget, depthStencil);
-
-        // Set the viewport.
-        auto viewport = deviceResources->GetScreenViewport();
-        context->RSSetViewports(1, &viewport);
-
-        apple->draw();
-        snake->draw();
-
-        deviceResources->Present();
-    }
+    auto const hr = swapChain.Present();
+  }
 };
 
 int main(int, char **) {
-    ComInitialize com;
+  ComInitialize com;
 
-    factory = CreateFactory();
-    // CreateDeviceIndependentResources();
+  // CreateDeviceIndependentResources();
 
-    WNDCLASS wc = {};
-    wc.hCursor = LoadCursor(nullptr, IDC_ARROW);
-    // wc.hInstance = module;
-    wc.lpszClassName = L"window";
-    wc.style = CS_HREDRAW | CS_VREDRAW;
+  WNDCLASS wc = {};
+  wc.hCursor = LoadCursor(nullptr, IDC_ARROW);
+  // wc.hInstance = module;
+  wc.lpszClassName = L"window";
+  wc.style = CS_HREDRAW | CS_VREDRAW;
 
-    wc.lpfnWndProc = [](HWND window, UINT message, WPARAM wparam,
-                        LPARAM lparam) -> LRESULT {
-        if (WM_PAINT == message)
-        {
-            PAINTSTRUCT ps;
-            VERIFY(BeginPaint(window, &ps));
-            // Render(window);
-            EndPaint(window, &ps);
-            return 0;
-        }
-
-        if (WM_SIZE == message)
-        {
-            if (target && SIZE_MINIMIZED != wparam)
-            {
-                // ResizeSwapChainBitmap();
-                // Render(window);
-            }
-
-            return 0;
-        }
-
-        if (WM_DISPLAYCHANGE == message)
-        {
-            // Render(window);
-            return 0;
-        }
-
-        if (WM_DESTROY == message)
-        {
-            PostQuitMessage(0);
-            return 0;
-        }
-
-        return DefWindowProc(window, message, wparam, lparam);
-    };
-
-    RegisterClass(&wc);
-
-    RECT rect = { 0, 0, static_cast<LONG>(SCREEN_WIDTH), static_cast<LONG>(SCREEN_HEIGHT) };
-
-    AdjustWindowRect(&rect, WS_OVERLAPPEDWINDOW, FALSE);
-
-    auto width = rect.right - rect.left;
-    auto height = rect.bottom - rect.top;
-
-    auto window = CreateWindow(wc.lpszClassName, L"Snake written in C++ with dx.h",
-                 WS_OVERLAPPEDWINDOW | WS_VISIBLE, CW_USEDEFAULT, CW_USEDEFAULT,
-                 width, height, nullptr, nullptr, nullptr, nullptr);
-
-    auto game = std::make_unique<Game>();
-    if(game->Initialize(window, SCREEN_WIDTH, SCREEN_HEIGHT)) {
-        MSG message = {};
-        while (WM_QUIT != message.message)
-        {
-            if (PeekMessage(&message, nullptr, 0, 0, PM_REMOVE))
-            {
-                TranslateMessage(&message);
-                DispatchMessage(&message);
-            }
-            else
-            {
-                game->Tick();
-            }
-        }
+  wc.lpfnWndProc = [](HWND window, UINT message, WPARAM wparam,
+                      LPARAM lparam) -> LRESULT {
+    if (WM_PAINT == message) {
+      PAINTSTRUCT ps;
+      VERIFY(BeginPaint(window, &ps));
+      // Render(window);
+      EndPaint(window, &ps);
+      return 0;
     }
-    // ReleaseDevice();
-    factory.Reset();
-    game.reset();
+
+    // if (WM_SIZE == message) {
+    //   if (target && SIZE_MINIMIZED != wparam) {
+    //     // ResizeSwapChainBitmap();
+    //     // Render(window);
+    //   }
+
+    //   return 0;
+    // }
+
+    if (WM_DISPLAYCHANGE == message) {
+      // Render(window);
+      return 0;
+    }
+
+    if (WM_DESTROY == message) {
+      PostQuitMessage(0);
+      return 0;
+    }
+
+    return DefWindowProc(window, message, wparam, lparam);
+  };
+
+  RegisterClass(&wc);
+
+  RECT rect = {0, 0, static_cast<LONG>(SCREEN_WIDTH),
+               static_cast<LONG>(SCREEN_HEIGHT)};
+
+  AdjustWindowRect(&rect, WS_OVERLAPPEDWINDOW, FALSE);
+
+  auto width = rect.right - rect.left;
+  auto height = rect.bottom - rect.top;
+
+  auto window = CreateWindow(
+      wc.lpszClassName, L"Snake written in C++ with dx.h",
+      WS_OVERLAPPEDWINDOW | WS_VISIBLE, CW_USEDEFAULT, CW_USEDEFAULT, width,
+      height, nullptr, nullptr, nullptr, nullptr);
+
+  auto game = std::make_unique<Game>();
+  if (game->Initialize(window, SCREEN_WIDTH, SCREEN_HEIGHT)) {
+    MSG message = {};
+    while (WM_QUIT != message.message) {
+      if (PeekMessage(&message, nullptr, 0, 0, PM_REMOVE)) {
+        TranslateMessage(&message);
+        DispatchMessage(&message);
+      } else {
+        game->Tick();
+      }
+    }
+  }
+  // ReleaseDevice();
+  game.reset();
 }
