7162d09edb2775c09c1409bd55eea6ac27c288ac
Johan Lindfors <johan.lindfors@coderox.se>
2018-12-17 22:21:50 +0100

    Better matrices

--- cplusplus/opengl/src/main.cpp
+++ cplusplus/opengl/src/main.cpp
@@ -259,7 +259,7 @@ int main( void )
 	glfwSetInputMode(window, GLFW_STICKY_KEYS, GL_TRUE);
 
 	// Dark blue background
-	glClearColor(0.0f, 0.0f, 0.4f, 0.0f);
+	glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
 
 	GLuint VertexArrayID;
 	glGenVertexArrays(1, &VertexArrayID);
@@ -269,12 +269,12 @@ int main( void )
 	GLuint programID = LoadShaders( "shaders/SimpleShader.vert", "shaders/SimpleShader.frag" );
 
 	static const GLfloat g_vertex_buffer_data[] = { 
-		0.0f, 0.0f, 0.0f,
-		0.0f, 100.0f, 0.0f,
-		100.0f,  0.0f, 0.0f,
-		100.0f,  0.0f, 0.0f,
-		.0f, 100.0f, 0.0f,
-		100.0f, 100.0f, 0.0f,
+		1.0f, 1.0f, 0.0f,
+		1.0f, 20.0f, 0.0f,
+		20.0f,  1.0f, 0.0f,
+		20.0f,  1.0f, 0.0f,
+		1.0f, 20.0f, 0.0f,
+		20.0f, 20.0f, 0.0f,
 		};
 
 	GLuint vertexbuffer;
@@ -291,7 +291,37 @@ int main( void )
 		// Get a handle for our "MVP" uniform
 		// Only during the initialisation
 		GLuint MatrixID = glGetUniformLocation(programID, "MVP");
-		glm::mat4 mvp = glm::translate(glm::mat4(1.0f), glm::vec3(1.0f, -1.0f, 0.0f));
+		glm::mat4 orthomatrix;
+
+		auto right = 400;
+		auto left = 0;
+		auto top = 0;
+		auto bottom = 400;
+		auto Zfar = 10;
+		auto Znear = 0;
+
+		orthomatrix[0].x = 2.0/(right-left);
+        orthomatrix[0].y = 0;
+        orthomatrix[0].z = 0;
+        orthomatrix[0].w = 0;
+ 
+        orthomatrix[1].x = 0;
+        orthomatrix[1].y = 2.0/(top-bottom);
+        orthomatrix[1].z = 0;
+        orthomatrix[1].w = 0;
+ 
+        orthomatrix[2].x = 0;
+        orthomatrix[2].y = 0;
+        orthomatrix[2].z = 2.0/(Zfar-Znear);
+        orthomatrix[2].w = 0;
+ 
+        orthomatrix[3].x = -(right+left)/(right-left);
+        orthomatrix[3].y = -(top+bottom)/(top-bottom);
+        orthomatrix[3].z = -(Zfar+Znear)/(Zfar-Znear);
+        orthomatrix[3].w = 1;
+
+    glm::mat4 mvp = orthomatrix * glm::translate(glm::mat4(1.0), glm::vec3(100.0f, 0.0f, 0.0f));
+
 		// Send our transformation to the currently bound shader, in the "MVP" uniform
 		// This is done in the main loop since each model will have a different MVP matrix (At least for the M part)
 		glUniformMatrix4fv(MatrixID, 1, GL_FALSE, &mvp[0][0]);
