78a3e8c13449c4ee42851a72cdda133a16c9cbcc
Johan Lindfors <Johan.Lindfors@coderox.se>
2019-10-25 07:15:59 +0200

    Now runs on OSX

--- rust/sdl2/build.rs
+++ rust/sdl2/build.rs
@@ -1,3 +1,7 @@
 fn main() {
-    println!("cargo:rustc-link-lib=shell32");
+    use std::env;
+    match env::var("CARGO_CFG_windows") {
+        Ok(_s) => println!("cargo:rustc-link-lib=shell32"),
+        Err(_err) => println!(""),
+    }
 }
