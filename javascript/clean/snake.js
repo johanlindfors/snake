3c4b4e612c894d20bfb218dacafad623e87a1682
Johan Lindfors // coderox <Johan.Lindfors@coderox.se>
2021-09-18 16:30:11 +0200

    A new version of clean js

--- javascript/clean/snake.js
+++ javascript/clean/snake.js
@@ -7,7 +7,7 @@ FPS = 15;
 var surface;
 var ctx;
 
-let apple = {
+let Apple = {
     x: 3,
     y: 3,
 
@@ -21,7 +21,7 @@ let apple = {
     }
 };
 
-let snake = {
+let Snake = {
     x: 10,
     y: 10,
     dx: 0,
@@ -41,17 +41,17 @@ let snake = {
                 }
                 this.x = this.y = 10;
                 this.dx = this.dy = 0;
-                this.tail = INITIAL_TAIL;        
+                this.tail = INITIAL_TAIL;
             }
-        });    
+        });
     },
 
     update: function() {
         this.x = (this.x + this.dx + SCREEN_SIZE) % SCREEN_SIZE;
         this.y = (this.y + this.dy + SCREEN_SIZE) % SCREEN_SIZE;
-    
+
         this.checkCollision();
-        
+
         this.trail.push({x:this.x, y:this.y});
         while(this.trail.length > this.tail){
             this.trail.shift();
@@ -66,73 +66,78 @@ let snake = {
     }
 };
 
+let game = {
+    snake: Snake,
+    apple: Apple,
+
+    generateApple: function() {
+        needNewApple = false;
+        do {
+            needNewApple = false;
+            this.apple.x = Math.floor(Math.random() * SCREEN_SIZE);
+            this.apple.y = Math.floor(Math.random() * SCREEN_SIZE);
+            this.snake.trail.forEach(element => {
+                needNewApple |= (element.x == this.apple.x && element.y == this.apple.y);
+            });
+        } while(needNewApple)
+    },
+
+    checkPickup: function() {
+        if(this.apple.x == this.snake.x && this.apple.y == this.snake.y){
+            this.snake.tail++;
+            this.generateApple();
+        }
+    },
+
+    update: function() {
+        this.snake.update();
+        this.checkPickup();
+    },
+
+    draw: function() {
+        ctx.fillStyle = "black";
+        ctx.fillRect(0, 0, surface.width, surface.height);
+
+        this.snake.draw();
+        this.apple.draw();
+    },
+}
+
 function keyPush(evt){
-    if(snake.dx == 0) {
+    if(game.snake.dx == 0) {
         switch(evt.keyCode){
             case 37:
-                snake.dx = -1;
-                snake.dy = 0;
+                game.snake.dx = -1;
+                game.snake.dy = 0;
                 break;
             case 39:
-                snake.dx = 1;
-                snake.dy = 0;
+                game.snake.dx = 1;
+                game.snake.dy = 0;
                 break;
         }
     }
-    if(snake.dy == 0){
+    if(game.snake.dy == 0){
         switch(evt.keyCode){
             case 38:
-            snake.dx = 0;
-            snake.dy = -1;
-            break;
-        case 40:
-            snake.dx = 0;
-            snake.dy = 1;
-            break;
+                game.snake.dx = 0;
+                game.snake.dy = -1;
+                break;
+            case 40:
+                game.snake.dx = 0;
+                game.snake.dy = 1;
+                break;
         }
     }
-}
-
-function generateApple() {
-    needNewApple = false;
-    do {
-        needNewApple = false;
-        apple.x = Math.floor(Math.random() * SCREEN_SIZE);
-        apple.y = Math.floor(Math.random() * SCREEN_SIZE);
-        snake.trail.forEach(element => {
-            needNewApple |= (element.x == apple.x && element.y == apple.y);
-        });
-    } while(needNewApple)
-}
-
-function checkPickup() {
-    if(apple.x == snake.x && apple.y == snake.y){
-        snake.tail++;
-        generateApple();
-    }
-}
-
-function update() {
-    snake.update();
-    checkPickup();
-}
-
-function draw() {
-    ctx.fillStyle = "black";
-    ctx.fillRect(0, 0, surface.width, surface.height);
-
-    snake.draw();
-    apple.draw();
 }
 
 function gameLoop() {
-    update();
-    draw();
+    game.update();
+    game.draw();
 }
 
 window.onload = function() {
     surface = document.getElementById("surface");
     ctx = surface.getContext("2d");
-    document.addEventListener("keydown",keyPush);
+    document.addEventListener("keydown", keyPush);
     setInterval(gameLoop, 1000 / FPS);
 }
