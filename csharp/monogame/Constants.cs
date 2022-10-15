bc0b90322414c2ec2cd0d178da794e75e33b45ab
Johan Lindfors <Johan.Lindfors@truesec.se>
2022-10-15 12:35:14 +0200

    .NET 6

--- csharp/monogame/Constants.cs
+++ csharp/monogame/Constants.cs
@@ -3,7 +3,7 @@ namespace Snake
     public static class Constants {
         public const int SPRITE_SIZE = 20;
         public const int SCREEN_HEIGHT = 20;
-        public const int SCREEN_WIDTH = 10;
+        public const int SCREEN_WIDTH = 20;
         public const int FPS = 15;
         public const int INITIAL_TAIL = 5;
     }
