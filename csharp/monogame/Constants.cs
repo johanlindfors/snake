6e4175246b6256627e16fa7c69c29638c8839766
Johan Lindfors <Johan.Lindfors@coderox.se>
2023-10-28 14:37:41 +0200

    Testing ssh

--- csharp/monogame/Constants.cs
+++ csharp/monogame/Constants.cs
@@ -1,9 +1,9 @@
 namespace Snake 
 {
     public static class Constants {
-        public const int SPRITE_SIZE = 20;
-        public const int SCREEN_HEIGHT = 20;
-        public const int SCREEN_WIDTH = 20;
+        public const int SPRITE_SIZE = 10;
+        public const int SCREEN_HEIGHT = 10;
+        public const int SCREEN_WIDTH = 10;
         public const int FPS = 15;
         public const int INITIAL_TAIL = 5;
     }
