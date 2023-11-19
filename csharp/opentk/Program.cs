07a74223e345573ae003ac7a7ffd80ee041abc03
Johan Lindfors <Johan.Lindfors@coderox.se>
2023-11-19 15:11:23 +0100

    Initial commit, renders a window

---
+++ csharp/opentk/Program.cs
@@ -0,0 +1,34 @@
+﻿using OpenTK.Graphics.OpenGL4;
+using OpenTK.Windowing.Common;
+using OpenTK.Windowing.Desktop;
+using OpenTK.Windowing.GraphicsLibraryFramework;
+
+namespace YourNamespaceHere
+{
+    public class Game : GameWindow
+    {
+        public Game(int width, int height, string title) : base(GameWindowSettings.Default, new NativeWindowSettings() { Size = (width, height), Title = title }) 
+        {
+
+        }
+
+        protected override void OnUpdateFrame(FrameEventArgs e)
+        {
+            base.OnUpdateFrame(e);
+
+            if (KeyboardState.IsKeyDown(Keys.Escape))
+            {
+                Close();
+            }
+        }
+
+        public static void Main(string[] args)
+        {
+            // This line creates a new instance, and wraps the instance in a using statement so it's automatically disposed once we've exited the block.
+            using (Game game = new Game(800, 600, "LearnOpenTK"))
+            {
+                game.Run();
+            }
+        }
+    }
+}
